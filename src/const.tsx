8168a7696949d68197a1cc8302a4ae1886fcd22b
kozyrev-dv <k-daniilv@mail.ru>
2024-04-06 13:31:18 +0300

    reverted to previous structure of consts

--- src/const.tsx
+++ src/const.tsx
@@ -4,6 +4,10 @@ export const URL_MARKER_DEFAULT =
 export const URL_MARKER_CURRENT =
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
 
+export const Settings = {
+  placesCount: 250,
+} as const;
+
 export enum CardType {
   favourites = 'favorites__card',
   nearest = 'near-places__card',
