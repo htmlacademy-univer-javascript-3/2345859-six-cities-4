1182190af75afe13faab602a3b6bdf93fc9b988c
kozyrev-dv <k-daniilv@mail.ru>
2024-05-23 19:03:55 +0300

    mocks updated

--- src/mocks/offers.ts
+++ src/mocks/offers.ts
@@ -23,7 +23,7 @@ export const offers: Offer[] = [
     isPremium: true,
     rating: 4,
     previewImage:
-      'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2l0eXxlbnwwfHwwfHx8MA%3D%3D',
+      'https://w.forfun.com/fetch/85/85ae14568a42dcfeb55be68307d1a797.jpeg',
   },
   {
     id: '2',
@@ -47,7 +47,7 @@ export const offers: Offer[] = [
     isPremium: false,
     rating: 3,
     previewImage:
-      'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2l0eXxlbnwwfHwwfHx8MA%3D%3D',
+      'https://w.forfun.com/fetch/85/85ae14568a42dcfeb55be68307d1a797.jpeg',
   },
   {
     id: '3',
@@ -71,7 +71,7 @@ export const offers: Offer[] = [
     isPremium: false,
     rating: 4.24,
     previewImage:
-      'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2l0eXxlbnwwfHwwfHx8MA%3D%3D',
+      'https://w.forfun.com/fetch/85/85ae14568a42dcfeb55be68307d1a797.jpeg',
   },
   {
     id: '4',
@@ -95,6 +95,30 @@ export const offers: Offer[] = [
     isPremium: false,
     rating: 2,
     previewImage:
-      'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2l0eXxlbnwwfHwwfHx8MA%3D%3D',
+      'https://w.forfun.com/fetch/85/85ae14568a42dcfeb55be68307d1a797.jpeg',
+  },
+  {
+    id: '5',
+    title: 'Beautiful & luxurious studio at great location',
+    type: 'apartment',
+    price: 120,
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 52.35514938496378,
+        longitude: 4.673877537499948,
+        zoom: 8,
+      },
+    },
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 8,
+    },
+    isFavorite: true,
+    isPremium: false,
+    rating: 2,
+    previewImage:
+      'https://w.forfun.com/fetch/85/85ae14568a42dcfeb55be68307d1a797.jpeg',
   },
 ];
